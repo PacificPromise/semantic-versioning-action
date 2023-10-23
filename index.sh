8d343c0cdc1f60f7db8971fa6f511c3e7b7d1478
tuanngocptn <tuanngocptn@gmail.com>
2023-10-23 15:34:11 +0700

    feat: build code

--- index.sh
+++ index.sh
@@ -21,6 +21,13 @@ remove_all_tag() {
 health_check() {
   echo "Semantic Version Action (SVA) is Ok"
 }
+
+config_github_token() {
+  REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  git remote add publisher $REMOTE_REPO
+  git config user.name bump_version
+  git config user.email bump-version@version.com
+}
 split_version() {
   TAG_NAME=$1
   VERSION_TYPE=$2 # full, all, major, minor, patche, increment_patche, build, increment_build
