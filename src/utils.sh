b6bb3aad4c2b9e91bd6408dbc1c17a9fce235bff
tuanngocptn <tuanngocptn@gmail.com>
2023-10-23 15:31:49 +0700

    feat: add config_github_token

--- src/utils.sh
+++ src/utils.sh
@@ -1,3 +1,10 @@
 health_check() {
   echo "Semantic Version Action (SVA) is Ok"
 }
+
+config_github_token() {
+  REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  git remote add publisher $REMOTE_REPO
+  git config user.name bump_version
+  git config user.email bump-version@version.com
+}
