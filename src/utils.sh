a375125f550f7c65ff4d019dba2f461d49704bf8
tuanngocptn <tuanngocptn@gmail.com>
2023-10-24 17:12:06 +0700

    fix: change remote url

--- src/utils.sh
+++ src/utils.sh
@@ -4,7 +4,8 @@ health_check() {
 
 config_github_token() {
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-  git remote add publisher $REMOTE_REPO
+  # git remote add publisher $REMOTE_REPO
+  git remote remote set-url origin $REMOTE_REPO
   git config user.name "GitHub Actions"
   git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 }
