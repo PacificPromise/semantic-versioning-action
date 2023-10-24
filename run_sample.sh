716c31c4228f8273e31089c46cb5720a4ed589b9
tuanngocptn <tuanngocptn@gmail.com>
2023-10-25 01:11:29 +0700

    feat: add sample and change tuanngocptn to PacificPromise

--- run_sample.sh
+++ run_sample.sh
@@ -1,1 +1,1 @@
-source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/tuanngocptn/semantic-versioning-action/main/index.sh)" && get_stage_prompt
+source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/PacificPromise/semantic-versioning-action/main/index.sh)" && get_stage_prompt
