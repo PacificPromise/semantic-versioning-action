d4e7896c6b71c5b3d6eb8913e2b221703de22e52
Nick - Ngoc Pham <tuanngocptn@users.noreply.github.com>
2024-08-30 16:57:39 +0700

    chore: add author

---
+++ src/author.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Filename:        index.sh
+# Author:          Nick (Ngoc Pham)
+# Created:         2023-10-23
+# Last Updated:    2024-08-30
+# Description:     Semantic versioning without any track files.
+# Version:         1.0
+# Usage:           Read at https://github.com/PacificPromise/semantic-versioning-action
+# Notes:           This action uses git tag for controlling the version. This action will not change/add/remove any file in your repository.
+# -----------------------------------------------------------------------------
